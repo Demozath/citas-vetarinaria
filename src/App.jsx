cdf57d2b88084def9da59b3a41203d0e3c72859b
Demozath <abeljsmz@gmail.com>
2023-08-13 00:28:08 -0400

    Se realizan algunas modificaciones

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,21 @@
 function App() {
+  
+  const sumar = () => {
+
+    const numero = 8;
+
+    if(numero > 5){
+      console.log('Si es mayor')
+    } else{
+      console.log('No es mayor')
+    }
+  }
+  sumar()
   return (
-     <div className="App">
-      <h1>Hola mundo</h1>
-      </div>
+     <>
+        <h1>Hola mundo</h1>
+        
+     </>
   )
 }
 
